7bf9b5477da94197a67b4dee7f7d6665ce17b0ae
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2026-05-17 09:01:13 +0300

    Update ruRU.lua

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -2,3 +2,20 @@
 local _, ns = ...
 if GetLocale() ~= "ruRU" then return end
 local L = ns.L
+
+L["Everything Quests"] = "Everything Quests"
+L["General"]              = "Общие"
+L["Tracker"]              = "Трекер"
+L["Quest Log"]            = "Журнал заданий"
+L["World Quests"]         = "Локальные задания"
+L["Chain Guide"]          = "Гайд по цепочкам"
+L["Appearance"]           = "Внешний вид"
+L["Open Quest Log"]       = "Открыть журнал заданий"
+L["Options"]              = "Настройки"
+L["Simplify Mode"]        = "Упрощенный режим"
+L["Sort by zone"]         = "Сортировка по зонам"
+L["Sort by status"]       = "Сортировка по статусу"
+L["Sort by type"]         = "Сортировка по типу"
+L["Sort by level"]        = "Сортировка по уровню"
+L["Sort by distance"]     = "Сортировка по расстоянию"
+L["Sort manually (drag)"] = "Вручную (перетаскиванием)"
